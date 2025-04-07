43772afc0243db5ede13cdccaa71cc87da81e793
UnB0tMas <tresdynx@gmail.com>
2025-04-06 21:13:40 -0500

    Add files via upload

---
+++ APPPSS/src/main/java/com/franco/apppss/ApppssApplication.java
@@ -0,0 +1,13 @@
+package com.franco.apppss;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ApppssApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(ApppssApplication.class, args);
+    }
+
+}
---
+++ APPPSS/src/test/java/com/franco/apppss/ApppssApplicationTests.java
@@ -0,0 +1,13 @@
+package com.franco.apppss;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class ApppssApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
